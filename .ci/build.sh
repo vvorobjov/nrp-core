72b01ebb00e44b8765a0049e6ddf30f3a2749282
Krzysztof Lebioda <krzysztof.lebioda@tum.de>
2021-04-29 14:58:43 +0000

    Merged in NRRPLT-8178-limit-build-resources (pull request #23)
    [NRRPLT-8178] Limit resources available to the build
    
    * [NRRPLT-8178] Limit resources available to the build
    
    Approved-by: Eloy Retamino
    Approved-by: Viktor Vorobev

--- .ci/build.sh
+++ .ci/build.sh
@@ -22,7 +22,23 @@ if [ -z "$NRP_INSTALL_DIR" ]; then
 fi
 
 cmake .. -DCMAKE_INSTALL_PREFIX="$NRP_INSTALL_DIR"
-make -j
+
+NPROC=$(nproc)
+
+# By default, limit jobs by 70% of cores,
+# or up to 100% if we have few cores:
+#    if 70% is less than 4, then use 4 parallel jobs
+#    if the $NPROC is less than 4, use -j${NPROC}
+
+J_LIM=$(python3 -c "print(max(${NPROC} * 7 // 10, min(${NPROC}, 4)))")
+
+# Limit job scheduling by 100% of CPU load
+# if 1min or 5min CPU load is higher than 100%, wait with job scheduling
+
+L_LIM="${NPROC}"
+
+echo "Running make with -j${J_LIM} -l${L_LIM}"
+make -j"${J_LIM}" -l"${L_LIM}"
 make install
 
 # Run all tests. Save test results in build/xml directory (used by Jenkins).
