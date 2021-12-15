98f155475dc2aede762d9bafbdcca0434803830e
erdisayar <erdisayar@hotmail.com>
2021-12-15 15:21:10 +0000

    [NRRPLT-8228] TF Update Unity Example

--- python_engine/tf_1.py
+++ python_engine/tf_1.py
@@ -4,10 +4,10 @@ from nrp_core import *
 from nrp_core.data.nrp_json import *
 from nrp_core.data.nrp_protobuf import *
 
-def get_info(info):
-    print("Board position: " + str(info.board_position))
-    print("Board rotation: " + str(info.board_rotation))
-    print("Joint angles: " + str(info.joint_angles))
+def get_info_temp(info):
+    print("Board position: " + str(info.boardPosition[:]))
+    print("Board rotation: " + str(info.boardRotation[:]))
+    print("Joint angles: " + str(info.boardCraneJointAngles[:]))
 
 def get_camera(camera, i):
     _retrieve_image(camera, i)
@@ -16,7 +16,7 @@ def _retrieve_image(reply, i):
     # how to access the image data from reply.imageData and store it in a file, as an example
     # create folder "images" in the python workind directory and uncomment the lines below if you want to save the file
     with open('images/received-image-{}.{}'.format(i, 'jpg'), 'wb') as image_file:
-        image_file.write(reply.image_data)
+        image_file.write(reply.imageData)
     pass
 
 iteration = 0
@@ -27,13 +27,18 @@ iteration = 0
 def transceiver_function(info, camera):
 
     device1 = JsonDataPack("device1", "python")
-
+    global iteration
     # Process data coming from the game
     if not info.isEmpty():
+        get_info_temp(info.data)
+        get_camera(camera.data, iteration)
         print(info.engine_name)
         print(info.type)
+        iteration += 1
     else:
         print("EMPTY")
+    
+    
     # global iteration
     # get_info(info)
     # get_camera(camera, iteration)
