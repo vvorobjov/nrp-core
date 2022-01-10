e4fa7ce706eb2bd34bc377c7927cbd1c718e48d4
Krzysztof Lebioda <krzysztof.lebioda@tum.de>
2022-01-10 15:34:14 +0100

    [NRRPLT-8453] Add possibility to load arbitrary Script class from Python engine

---
+++ examples/python/engine.py
@@ -0,0 +1,34 @@
+import cv2
+
+def preprocess_image(iteration): #-> np.ndarray:
+    
+    # Load image of the current step
+    frame = cv2.imread('./images/received-image-' + str(iteration) + '.jpg')
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # If resizing is need
+    frame = cv2.resize(frame, (150, 150), interpolation=cv2.INTER_AREA)
+    # Preprocessing methods:
+
+    # method 1: Edge detection
+    frame = cv2.Canny(frame, 35, 100)
+    # method 2: Threshold
+    #frame = cv2.threshold(frame, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    return frame[..., None]
+
+
+from stable_baselines3 import A2C, PPO
+
+class Script():
+    def initialize(self):
+        print("Python engine is initializing. Registering datapack...")
+        agent_name_g = "PPO_agent_canny_image_cnn_policy_150"
+        log_dir_g = "./scripts/python_nrp/sb3/agent_models/" + agent_name_g
+        self.model_g = PPO.load(log_dir_g + str(0))#, env)
+ 
+    def runLoop(self):
+        img = preprocess_image(1)
+        self.model_g.predict(img, deterministic=True)
+ 
+    def shutdown(self):
+        print("Python engine is shutting down")
--- nrp_python/server.py
+++ nrp_python/server.py
@@ -1,61 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from importlib import import_module
+
 app = Flask(__name__)
 
 engine_time = 0
 timestep = 200000000
 
+import os
+import sys
 
-import numpy as np
-import cv2
-
-def preprocess_image(iteration): #-> np.ndarray:
-    
-    # Load image of the current step
-    frame = cv2.imread('./images/received-image-' + str(iteration) + '.jpg')
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    # If resizing is need
-    frame = cv2.resize(frame, (150, 150), interpolation=cv2.INTER_AREA)
-    # Preprocessing methods:
-
-    # method 1: Edge detection
-    frame = cv2.Canny(frame, 35, 100)
-    # method 2: Threshold
-    #frame = cv2.threshold(frame, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    return frame[..., None]
-
-
-from stable_baselines3 import A2C, PPO
-
-class Script():
-    def initialize(self):
-        print("Python engine is initializing. Registering datapack...")
-        agent_name_g = "PPO_agent_canny_image_cnn_policy_150"
-        log_dir_g = "./scripts/python_nrp/sb3/agent_models/" + agent_name_g
-        self.model_g = PPO.load(log_dir_g + str(0))#, env)
- 
-    def runLoop(self):
-        img = preprocess_image(1)
-        self.model_g.predict(img, deterministic=True)
- 
-    def shutdown(self):
-        print("Python engine is shutting down")
-
-script = Script()
-
+script = None
 
 @app.route('/initialize', methods=["POST"])
 def initialize():
+    # TODO Import python engine module
+
+    script_filename = request.json["PythonFileName"]
+    script_dirname = os.path.dirname(script_filename)
+    script_basename = os.path.basename(script_filename)
+
+    # Append path of the script to sys.path. This is needed for the import to work
+    sys.path.append(script_dirname)
+    script_module = import_module(script_basename[:len(script_basename) - 3])
+
     global script
+    script = script_module.Script()
+
     script.initialize()
     return jsonify({"InitExecStatus": True})
 
 @app.route('/run_loop', methods=["POST"])
 def run_loop():
-    global script
-    script.runLoop()
     global engine_time
     engine_time = engine_time + timestep
+    script.runLoop()
     return jsonify({"time": engine_time})
 
 @app.route('/set_datapack', methods=["POST"])
@@ -72,6 +50,7 @@ def reset():
 
 @app.route('/shutdown', methods=["POST"])
 def shutdown():
+    script.shutdown()
     return {}
 
 # EOF
