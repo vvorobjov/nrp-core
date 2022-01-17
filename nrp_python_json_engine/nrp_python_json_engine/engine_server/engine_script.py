87bdb93115e44c1c81632bd85377cd9b7568872a
Krzysztof Lebioda <krzysztof.lebioda@tum.de>
2022-01-17 16:26:14 +0100

    [NRRPLT-8453] Improve implementation of EngineScript

--- nrp_python_json_engine/nrp_python_json_engine/engine_server/engine_script.py
+++ nrp_python_json_engine/nrp_python_json_engine/engine_server/engine_script.py
@@ -1,14 +1,28 @@
 class EngineScript:
+
     def __init__(self) -> None:
-        self.datapacks = {}
+        self._datapacks = {}
+
 
     def _registerDataPack(self, datapack_name):
-        self.datapacks[datapack_name] = None
+        if(datapack_name in self._datapacks):
+            raise Exception("DataPack with specified name (" + datapack_name + ") already registered")
+
+        self._datapacks[datapack_name] = None
+
 
     def _getDataPack(self, datapack_name):
-        return self.datapacks[datapack_name]
+        if(datapack_name not in self._datapacks):
+            raise Exception("Attempting to get data from an unregistered DataPack (" + datapack_name + ")")
+
+        return self._datapacks[datapack_name]
+
 
     def _setDataPack(self, datapack_name, data):
-        self.datapacks[datapack_name] = data
+        if(datapack_name not in self._datapacks):
+            raise Exception("Attempting to set data on an unregistered DataPack (" + datapack_name + ")")
+
+        self._datapacks[datapack_name] = data
+
 
 # EOF
--- nrp_python_json_engine/tests/test_engine_script.py
+++ nrp_python_json_engine/tests/test_engine_script.py
@@ -5,45 +5,80 @@ from nrp_core.engines.python_json.engine_script import EngineScript
 class Script(EngineScript):
     def initialize(self):
         pass
- 
+
     def runLoop(self):
         pass
- 
+
     def shutdown(self):
         pass
 
 class TestServer(unittest.TestCase):
 
-    def test_instantiate(self):
-        script = Script()
-        script.initialize()
-        script.runLoop()
-        script.shutdown()
-
     def test_datapack_register(self):
+        """
+        Try to register a datapack with given name.
+        The datapack should be added to the _datapacks dict.
+        """
         script = Script()
         datapack_name = "testDatapack"
         script._registerDataPack(datapack_name)
-        self.assertEqual(len(script.datapacks), 1)
-        self.assertEqual(script.datapacks[datapack_name], None)
+        self.assertEqual(len(script._datapacks), 1)
+        self.assertEqual(script._datapacks[datapack_name], None)
+
+
+    def test_datapack_register_overwrite(self):
+        """Try to register the same datapack twice. Should raise an exception"""
+        script = Script()
+        datapack_name = "testDatapack"
+        script._registerDataPack(datapack_name)
+        with self.assertRaises(Exception):
+            script._registerDataPack(datapack_name)
+        self.assertEqual(len(script._datapacks), 1)
+
 
     def test_datapack_set(self):
+        """Try to set data on a registered datapack"""
         script = Script()
         datapack_name = "testDatapack"
         data = {"test": 1}
         script._registerDataPack(datapack_name)
-
         script._setDataPack(datapack_name, data)
-        self.assertEqual(script.datapacks[datapack_name], data)
+        self.assertEqual(script._datapacks[datapack_name], data)
+
+
+    def test_datapack_set_unregistered(self):
+        """Try to set data on a unregistered datapack. Should raise an exception."""
+        script = Script()
+        datapack_name = "testDatapack"
+        data = {"test": 1}
+        with self.assertRaises(Exception):
+            script._setDataPack(datapack_name, data)
+        self.assertEqual(len(script._datapacks), 0)
+
 
     def test_datapack_get(self):
+        """
+        Try to get data from a registered datapack.
+        Should return None if the data was never set, and the latest data otherwise.
+        """
         script = Script()
         datapack_name = "testDatapack"
         data = {"test": 1}
         script._registerDataPack(datapack_name)
-
+        self.assertEqual(script._getDataPack(datapack_name), None)
         script._setDataPack(datapack_name, data)
         self.assertEqual(script._getDataPack(datapack_name), data)
 
+
+    def test_datapack_get_unregistered(self):
+        """Try to get data from an unregistered datapack. Should raise an exception."""
+        script = Script()
+        datapack_name = "testDatapack"
+        with self.assertRaises(Exception):
+            script._getDataPack(datapack_name)
+
+
 if __name__ == '__main__':
     unittest.main()
+
+# EOF
