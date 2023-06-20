7f0ef7a18fd0b61938cd8d16888dd8a31a5230b9
Eloy Retamino <retamino@ugr.es>
2023-06-20 10:58:09 +0200

    [NRRPLT-0000] Updated copyright notice, nrp-core version

--- nrp_python_json_engine/nrp_python_json_engine/engine_server/async_python_json_engine.py
+++ nrp_python_json_engine/nrp_python_json_engine/engine_server/async_python_json_engine.py
@@ -2,7 +2,7 @@
 
 # NRP Core - Backend infrastructure to synchronize simulations
 #
-# Copyright 2020-2021 NRP Team
+# Copyright 2020-2023 NRP Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
