2b2f05f7de33e10e1d21189fa7f46f397e98d0c6
Viktor Vorobev <vorobev@in.tum.de>
2023-06-26 15:58:33 +0000

    Merged in NRRPLT-8940-xpra-doesn-t-stop-child-processes-on-sigterm (pull request #199)
    [NRRPLT-8940] Fix xpra entrypoint behaviour on SIGTERM
    
    * [NRRPLT-8940] Fix xpra entrypoint behaviour on SIGTERM
    
    * [NRRPLT-8940] Fix xpra entrypoint behaviour on SIGTERM
    
    * Merged development into NRRPLT-8940-xpra-doesn-t-stop-child-processes-on-sigterm
    * [NRRPLT-8940] reduce the xvfb size in xpra
    
    
    Approved-by: Ugo Albanese
    Approved-by: Eloy Retamino

--- .ci/xpra-entrypoint.sh
+++ .ci/xpra-entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    #
+    pkill -SIGINT -e -P "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+# on callback, execute the specified handler
+trap 'term_handler' SIGTERM
+
 
 export NRP_DISPLAY="${NRP_DISPLAY:-100}"
 export NRP_XPRA_PORT="${NRP_XPRA_PORT:-9876}"
@@ -13,4 +29,7 @@ set -x
 # shellcheck disable=SC2001
 CHILD_CMD=$(echo "$*" | sed 's|"|\\"|g')
 
-xpra start --bind-tcp=0.0.0.0:"${NRP_XPRA_PORT}" --html=on --start-child="$CHILD_CMD" --exit-with-children --daemon=no ":${NRP_DISPLAY}"
+# run xpra
+xpra start --bind-tcp=0.0.0.0:"${NRP_XPRA_PORT}" --html=on --start-child="$CHILD_CMD" --exit-with-children --resize-display=1920x1080 --pulseaudio=no --terminate-children=yes --daemon=no ":${NRP_DISPLAY}" &
+pid="$!"
+wait ${pid}
